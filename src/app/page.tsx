22b19c68e165a2b82e7f07c8fb70a862556aaef3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-08 13:15:16 -0500

    Add Heading component to page to replace h2 from skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import Heading from '@/components/ui/Heading';
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold underline">
-        Welcome to the Run Book
-      </h1>
-      {/* Here you will include other components like RunList, etc. */}
+    <div className="container  flex flex-col justify-center">
+     <div className='text-center'>
+       <Heading title="the Run Book 2024" />
+     </div>
+      {/* Here I will include other components like RunList, etc. */}
     </div>
   );
 }
