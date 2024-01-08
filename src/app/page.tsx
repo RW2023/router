0483a8b233aa76596d9d45da07c7b4fa43c284c0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-08 13:19:19 -0500

    Update page layout and styling. set container min h screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="container  flex flex-col justify-center">
-     <div className='text-center'>
+    <div className="container  flex flex-col justify-center min-h-screen">
+     <div className='text-center justify-center items-center'>
        <Heading title="the Run Book 2024" />
      </div>
       {/* Here I will include other components like RunList, etc. */}
