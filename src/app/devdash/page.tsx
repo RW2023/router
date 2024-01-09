0518535c04de9f7f89b9de29f528c159a35d789b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-09 11:32:39 -0500

    Add Driver form and SubHeading component

--- src/app/devdash/page.tsx
+++ src/app/devdash/page.tsx
@@ -2,6 +2,8 @@
 import { FC } from 'react';
 import Heading from '@/components/ui/Heading';
 import DevDash from '@/components/DevDash';
+import Driver from '@/components/forms/Driver';
+import SubHeading from '@/components/ui/SubHeading';
 
 interface Props {}
 
@@ -10,6 +12,8 @@ const page: FC<Props> = (props): JSX.Element => {
     <div className='m-3'>
         <Heading title="Dev Dash" />
         <DevDash />
+        <SubHeading title="Driver Form" />
+        <Driver />
     </div>
   );
 };
