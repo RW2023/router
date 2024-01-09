005afc69b7d2db65b513d7ba2dd14d11bbbd3345
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-09 13:07:37 -0500

    Refactor home page layout and add login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 import Heading from '@/components/ui/Heading';
-import Image from 'next/image'
+import SubHeading from '@/components/ui/SubHeading';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container  flex flex-col justify-center min-h-screen">
-     <div className='text-center justify-center items-center'>
-       <Heading title="the Run Book 2024" />
-     </div>
-      {/* Here I will include other components like RunList, etc. */}
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url('/img/hero4.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      data-theme="business"
+    >
+      <div className="hero-overlay bg-opacity-10"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <div className="bg-base-200 rounded-md  mx-auto p-2 m-1 border border-1">
+            <Heading title="Run Book 2024" />
+            <SubHeading title="route information application" />
+          </div>
+          <Link
+          href="/devdash"
+          >
+            <button
+              type="submit"
+              className="btn btn-primary bg-button text-black"
+            >
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
