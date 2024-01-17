5838d30dd08feb37c8f072e15614dedd429220c9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-17 04:41:54 -0500

    add inline loading component

---
+++ src/components/ui/LoadingInline.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="text-center">
+        <p className="text-3xl">
+          <span className="m-2">
+            <span className="loading loading-bars loading-lg"></span>
+          </span>{' '}
+          Loading.....the details ...☝🏾...⏱️
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
